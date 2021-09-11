ae7d39ecf6bd2fb70e9d70061340ffccc157b4e4
Mash Codee <codeemash@gmail.com>
2021-09-11 20:52:49 +0530

    resolving cookie issue

--- src/api/graphql/resolvers/mutations/Logout.js
+++ src/api/graphql/resolvers/mutations/Logout.js
@@ -5,7 +5,13 @@ async function Logout({ args, User, res }) {
         const isUser = await User.findById(id)
         if (!isUser) return { success: false, message: "User does not exist!" }
         if (isUser.secret !== secret) return { success: false, message: "Wrong Secret!" }
-        res.clearCookie('refreshToken', { path: "/", maxAge: 0 })
+        res.cookie('refreshToken', null, {
+            maxAge: 0,
+            secure: true,
+            path: '/',
+            httpOnly: true,
+            sameSite: 'none'
+        })
 
         return { success: true, message: "User Logged out!" }
     } catch (err) {
