123daad94c003c057d5cf4574cc594f7a1152479
Mash Codee <codeemash@gmail.com>
2021-09-16 15:16:10 +0530

    little ui changes

--- src/api/routes/profileImageUpload.js
+++ src/api/routes/profileImageUpload.js
@@ -7,27 +7,27 @@ const fs = require('fs')
 
 function profileimageupload(app) {
     app.post('/profileimageupload', async (req, res) => {
-        try {
-            return res.json({ success: true, message: "This feature is not available yet" })
-            const { filename } = req.file
-            const userId = req.body.id
+        return res.json({ success: true, message: "This feature is not available yet" })
+        // try {
+        //     const { filename } = req.file
+        //     const userId = req.body.id
 
-            const isUser = await User.findById(userId)
-            if (!isUser) return res.json({ success: false, message: "User does not exist" })
-            const url = `https://chatsmanapi.herokuapp.com/${filename}`
+        //     const isUser = await User.findById(userId)
+        //     if (!isUser) return res.json({ success: false, message: "User does not exist" })
+        //     const url = `https://chatsmanapi.herokuapp.com/${filename}`
 
-            if (!!isUser.image) {
-                const alreadyExistImageName = isUser.image.substring(34)
-                fs.unlinkSync(`./src/images/${alreadyExistImageName}`)
-            }
+        //     if (!!isUser.image) {
+        //         const alreadyExistImageName = isUser.image.substring(34)
+        //         fs.unlinkSync(`./src/images/${alreadyExistImageName}`)
+        //     }
 
-            isUser.image = url
-            await isUser.save()
+        //     isUser.image = url
+        //     await isUser.save()
 
-            res.json({ success: true, message: "Profile Picture changed." })
-        } catch (err) {
-            res.json({ success: false, message: "There is some server error, try again later." })
-        }
+        //     res.json({ success: true, message: "Profile Picture changed." })
+        // } catch (err) {
+        //     res.json({ success: false, message: "There is some server error, try again later." })
+        // }
     })
 }
 
