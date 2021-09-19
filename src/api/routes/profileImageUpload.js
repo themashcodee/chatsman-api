ba82975b3c0860caa9d464bb0068611e3468bc57
Mash Codee <codeemash@gmail.com>
2021-09-19 12:15:14 +0530

    secret key varible problem fixing

--- src/api/routes/profileImageUpload.js
+++ src/api/routes/profileImageUpload.js
@@ -8,7 +8,7 @@ const upload = multer.single('file')
 
 const storage = new Storage({
     projectId: process.env.GCP_PROJECT,
-    credentials: { client_email: process.env.GCP_CLIENT_EMAIL, private_key: process.env.GCP_PRIVATE_KEY }
+    credentials: { client_email: process.env.GCP_CLIENT_EMAIL, private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n') }
 });
 const bucket = storage.bucket(process.env.GCP_BUCKET);
 
