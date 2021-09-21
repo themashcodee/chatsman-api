66eb07e7e5cbbed373d69598400de61d62b180ff
Mash Codee <codeemash@gmail.com>
2021-09-21 18:21:38 +0530

    cors policy fixed

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 const corsOption = {
-    origin: ['https://chatsman.vercel.app', 'http://localhost:3000', 'https://studio.apollographql.com'],
+    origin: ['https://chatsman.vercel.app', 'http://localhost:3000'],
     //  'https://studio.apollographql.com'
     credentials: true,
 }
